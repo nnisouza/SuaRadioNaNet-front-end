057dc4733eb63d94153470fa8558c3746dfcda1c
Giovani <giov@nnisouza.com>
2015-03-05 16:25:18 -0300

    adjusts

--- js/main.js
+++ js/main.js
@@ -1,38 +1,37 @@
 $(document).ready(function() {
+    var audio = new Audio();
+    audio.src = 'sound/gravity.m4a';
+    audio.controls = false;
+    audio.loop = true;
+    audio.autoplay = true;
+    var canvas, ctx, source, context, analyser, fbc_array, bars, bar_x, bar_width, bar_height;
+    window.addEventListener("load", initPlayer, false);
 
-var audio = new Audio();
-audio.src = 'sound/gravity.m4a';
-audio.controls = false;
-audio.loop = true;
-audio.autoplay = true;
-var canvas, ctx, source, context, analyser, fbc_array, bars, bar_x, bar_width, bar_height;
-window.addEventListener("load", initPlayer, false);
-
-function initPlayer(){
-	document.getElementById('audio_box').appendChild(audio);
-	context = new AudioContext();
-	analyser = context.createAnalyser();
-	canvas = document.getElementById('analyser_render');
-	ctx = canvas.getContext('2d');
-	source = context.createMediaElementSource(audio);
-	source.connect(analyser);
-	analyser.connect(context.destination);
-	frameLooper();
-}
-function frameLooper(){
-	window.requestAnimationFrame(frameLooper);
-	fbc_array = new Uint8Array(analyser.frequencyBinCount);
-	analyser.getByteFrequencyData(fbc_array);
-	ctx.clearRect(0, 0, canvas.width, canvas.height);
-	ctx.fillStyle = '#ffffff';
-	bars = 7;
-	for (var i = 0; i < bars; i++) {
-		bar_x = i * 45;
-		bar_width = 28;
-		bar_height = -(fbc_array[i] / 10 * 5.3);
-		ctx.fillRect(bar_x, canvas.height, bar_width, bar_height);
-	}
-}
+    function initPlayer(){
+        document.getElementById('audio_box').appendChild(audio);
+        context = new AudioContext();
+        analyser = context.createAnalyser();
+        canvas = document.getElementById('analyser_render');
+        ctx = canvas.getContext('2d');
+        source = context.createMediaElementSource(audio);
+        source.connect(analyser);
+        analyser.connect(context.destination);
+        frameLooper();
+    }
+    function frameLooper(){
+        window.requestAnimationFrame(frameLooper);
+        fbc_array = new Uint8Array(analyser.frequencyBinCount);
+        analyser.getByteFrequencyData(fbc_array);
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        ctx.fillStyle = '#ffffff';
+        bars = 7;
+        for (var i = 0; i < bars; i++) {
+            bar_x = i * 45;
+            bar_width = 28;
+            bar_height = -(fbc_array[i] / 10 * 5.3);
+            ctx.fillRect(bar_x, canvas.height, bar_width, bar_height);
+        }
+    }
 
 
 
